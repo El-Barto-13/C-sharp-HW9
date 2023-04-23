f1e9b07aad791255122baacf69353fd33d8b60dd
Alexis Smirnov <alexis.smirnoff@mail.ru>
2023-04-23 14:04:32 +0300

    домашка 9 конец

--- Program.cs
+++ Program.cs
@@ -10,15 +10,17 @@
 N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 */
-
-void ReduceNumber(int N)
+/*
+void decreaseNumber(int N)
 {
     Console.Write(N + " ");
-    if (N > 1) ReduceNumber(N - 1);
+    if (N > 1) decreaseNumber(N - 1);
 }
 Console.Write("Введите число N: ");
 int N = Convert.ToInt32(Console.ReadLine());
-ReduceNumber(N);
+decreaseNumber(N);
+*/
+
 
 
 /*
@@ -28,7 +30,29 @@ ReduceNumber(N);
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30
 */
+/*
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumSearch(M,N));
 
+int SumSearch(int m, int n)
+{
+    if (m!=n)
+    {
+        if (m > n)
+        {
+            return SumSearch(m - 1, n) + m;
+        }
+        else
+        {
+            return SumSearch(m + 1, n) + m;
+        }
+    }
+    return m;
+}
+*/
 
 
 
@@ -39,8 +63,20 @@ M = 4; N = 8. -> 30
 m = 2, n = 3 -> A(m,n) = 9
 m = 3, n = 2 -> A(m,n) = 29
 */
+/*
+Console.Write("Введите первое неотрицательное число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе неотрицательное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
 
 
-
+int Akkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return Akkerman(m - 1, 1);
+    else return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+Console.WriteLine(Akkerman(M, N));
+*/
 
 
